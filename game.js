5311123fbe845000ffc3eab7bc04491ab89e0799
Charlie Chen <chench2@whs.school.nz>
2022-06-30 09:29:08 +1200

    added comments

--- game.js
+++ game.js
@@ -1,18 +1,29 @@
 /**
-* Title: Movement control excercises
+* Title: Game design
 * Author: Charlie Chen
-* Date: 
-* Version: 1
-* Purpose: Design snake game
+* Date: 08/07/2022
+* Purpose: Design game
 **/
 
 // Some constants that will be used in this scripts
+// Width of the canvas 
 const WIDTH = 1000;
+// Width of the canvas
 const HEIGHT = 800;
 // SqaureWidth is the size of the square (40px * 4px) that the canvas is divided to (25squares * 20squares)
 const squareWidth = 40
+// Objects with all the colour thar we can access from
+const GAMECOLOUR = {
+    "playerColour": "#0000FF", 
+    "playerBorderColour": "#000000", 
+    "correctPaintColour": "#0000FF", 
+    "correctPaintBorderColour":"#000000",
+    "wrongPaintColour": "#7DF9FF", 
+    "wrongPaintBorderColour":"#000000"
+}
 
-// some variables that will be needed
+// Some variables that will be needed
+// The coordinates of the player on the canvas
 var player = [
     {x: squareWidth * 5, y: squareWidth * 3},
     {x: squareWidth * 4, y: squareWidth * 3},
@@ -20,35 +31,29 @@ var player = [
     {x: squareWidth * 3, y: squareWidth * 3},
     {x: squareWidth * 2, y: squareWidth * 3}
 ]
+// The canvas rendering context
 var ctx
+// The score of the game
 var score = 0;
-var myInterval;
-// Horizontal velocity
+// The game runs on the interval 
+var gameInterval;
+// Horizontal velocity of player
 var dx = squareWidth;
-// Vertical velocity
+// Vertical velocity of player
 var dy = 0;
-// healthy food x position 
-var healthy_food_x;
-// healthy food y position A
-var healthy_food_y;
-// poison food x position 
-var poison_food_x;
-// poison food y position 
-var poison_food_y;
-//objects with all the colour thar we can access from
-const gameColour = {
-"player_colour": "#0000FF", 
-"player_bordercolour": "#000000", 
-"healthy_food_colour": "#0000FF", 
-"healthy_food_bordercolour":"#000000",
-"pison_food_colour": "#7DF9FF", 
-"pison_food_bordercolour":"#000000"
-}
+// Correct paint x position 
+var correctPaintX;
+// Correct paint y position 
+var correctPaintY;
+// Wrong paint x position 
+var wrongPaintX;
+// Wrong paint y position 
+var wrongPaintY;
 
 
-
+//When content loads run the function startCanvas
 window.onload=startCanvas
-//start the game
+//This function is the starting screen of the game
 function startCanvas(){
     // Return a two dimensional drawing context
     ctx=document.getElementById("myCanvas").getContext("2d")
@@ -63,12 +68,12 @@ function startCanvas(){
     ctx.font = "70px comic"
     ctx.fillText("Click Anywhere to Start Game", WIDTH/2, HEIGHT*3/5);
 
-    ctx.fillStyle = gameColour["healthy_food_colour"];
-    ctx.strokestyle = gameColour["healthy_food_bordercolour"];
+    ctx.fillStyle = GAMECOLOUR["correctPaintColour"];
+    ctx.strokestyle = GAMECOLOUR["correctPaintBorderColour"];
     ctx.fillRect(940, 210, squareWidth, squareWidth);
     ctx.strokeRect(940, 210, squareWidth, squareWidth);
-    ctx.fillStyle = gameColour["pison_food_colour"];
-    ctx.strokestyle = gameColour["pison_food_bordercolour"];
+    ctx.fillStyle = GAMECOLOUR["wrongPaintColour"];
+    ctx.strokestyle = GAMECOLOUR["wrongPaintBorderColour"];
     ctx.fillRect(720, 290, squareWidth, squareWidth);
     ctx.strokeRect(720, 290, squareWidth, squareWidth);
 
@@ -85,7 +90,7 @@ function updateCanvas(){
     get_poison_food()
 
     //runs the game by making the snake move, and constantly check if it is dying 
-    myInterval = setInterval(()=> {
+    gameInterval = setInterval(()=> {
         if(has_game_ended() == true){
             gameEnd();
         } else {
@@ -102,7 +107,7 @@ function updateCanvas(){
 function gameEnd(){
     //stop the game from running
     clearCanvas(); 
-    clearInterval(myInterval)
+    clearInterval(gameInterval)
     console.log("you died!!!")
 
     ctx.font = "90px comic "
@@ -113,7 +118,7 @@ function gameEnd(){
 
     window.addEventListener('click',onClick);
     function onClick(){
-    document.location.reload();
+        document.location.reload();
     }
 }
 
@@ -133,58 +138,58 @@ function random_food_postion(min, max){
 
 
 function get_healthy_food() {
-     healthy_food_x = random_food_postion(0, WIDTH - squareWidth);
-     console.log(healthy_food_x)
-     healthy_food_y = random_food_postion(0, HEIGHT - squareWidth);
-     console.log(healthy_food_y)
+    correctPaintX = random_food_postion(0, WIDTH - squareWidth);
+    console.log(correctPaintX)
+    correctPaintY = random_food_postion(0, HEIGHT - squareWidth);
+    console.log(correctPaintY)
 
 //make sure that the healthy food is not on the player
-     player.forEach(function food_on_player (part) {
-            const on_player = (part.x == healthy_food_x) && (part.y == healthy_food_y)
-            if (on_player) {
-                get_healthy_food()
-            }
+    player.forEach(function food_on_player (part) {
+        const on_player = (part.x == correctPaintX) && (part.y == correctPaintY)
+        if (on_player) {
+            get_healthy_food()
+        }
     });
 }
 
 
 
 function get_poison_food() {
-    poison_food_x = random_food_postion(0, WIDTH - squareWidth);
-    console.log(poison_food_x)
-    poison_food_y = random_food_postion(0, HEIGHT - squareWidth);
-    console.log(poison_food_y)
+    wrongPaintX = random_food_postion(0, WIDTH - squareWidth);
+    console.log(wrongPaintX)
+    wrongPaintY = random_food_postion(0, HEIGHT - squareWidth);
+    console.log(wrongPaintY)
 
 //make sure that the poison food is not on the player
     player.forEach(function food_on_player (part) {
-           if ((part.x == poison_food_x) && (part.y == poison_food_y)) {
-               get_poison_food()
-           }
+        if ((part.x == wrongPaintX) && (part.y == wrongPaintY)) {
+           get_poison_food()
+        }
    });
 
 //make sure that the poison food is not too close to player
-    if ((Math.abs(player[0].x - poison_food_x) < squareWidth*5 ) || (Math.abs(player[0].y - poison_food_y) < squareWidth*5)) {
+    if ((Math.abs(player[0].x - wrongPaintX) < squareWidth*5 ) || (Math.abs(player[0].y - wrongPaintY) < squareWidth*5)) {
         console.log("too close")
         get_poison_food()
     }
 
 //makesure thatthe poison food is not on the healthy food 
-    if ((poison_food_x == healthy_food_x) && (poison_food_y == healthy_food_y)){
-    get_poison_food()
+    if ((wrongPaintX == correctPaintX) && (wrongPaintY == correctPaintY)){
+        get_poison_food()
     }
 }
 
 
 
 function drawFood() {
-    ctx.fillStyle = gameColour["healthy_food_colour"];
-    ctx.strokestyle = gameColour["healthy_food_bordercolour"];
-    ctx.fillRect(healthy_food_x, healthy_food_y, squareWidth, squareWidth);
-    ctx.strokeRect(healthy_food_x, healthy_food_y, squareWidth, squareWidth);
-    ctx.fillStyle = gameColour["pison_food_colour"];
-    ctx.strokestyle = gameColour["pison_food_bordercolour"];
-    ctx.fillRect(poison_food_x, poison_food_y, squareWidth, squareWidth);
-    ctx.strokeRect(poison_food_x, poison_food_y, squareWidth, squareWidth);
+    ctx.fillStyle = GAMECOLOUR["correctPaintColour"];
+    ctx.strokestyle = GAMECOLOUR["correctPaintBorderColour"];
+    ctx.fillRect(correctPaintX, correctPaintY, squareWidth, squareWidth);
+    ctx.strokeRect(correctPaintX, correctPaintY, squareWidth, squareWidth);
+    ctx.fillStyle = GAMECOLOUR["wrongPaintColour"];
+    ctx.strokestyle = GAMECOLOUR["wrongPaintBorderColour"];
+    ctx.fillRect(wrongPaintX, wrongPaintY, squareWidth, squareWidth);
+    ctx.strokeRect(wrongPaintX, wrongPaintY, squareWidth, squareWidth);
 }
 
 
@@ -194,14 +199,14 @@ function drawPlayer(){
 }
 function drawPlayerPart(part){
     // Set the colour of the player part
-     ctx.fillStyle = gameColour["player_colour"];
-     // Set the border colour of the player part
-     ctx.strokestyle = gameColour["player_bordercolour"];
-     // Draw a "filled" rectangle to represent the player part at the coordinates
-     // the part is located
-     ctx.fillRect(part.x, part.y, squareWidth, squareWidth);
-     // Draw a border around the player part
-     ctx.strokeRect(part.x, part.y, squareWidth, squareWidth);
+    ctx.fillStyle = GAMECOLOUR["playerColour"];
+    // Set the border colour of the player part
+    ctx.strokestyle = GAMECOLOUR["playerBorderColour"];
+    // Draw a "filled" rectangle to represent the player part at the coordinates
+    // the part is located
+    ctx.fillRect(part.x, part.y, squareWidth, squareWidth);
+    // Draw a border around the player part
+    ctx.strokeRect(part.x, part.y, squareWidth, squareWidth);
 }
 
 
@@ -249,7 +254,7 @@ function move_player() {
     // Add the new head to the beginning of snake body
     player.unshift(head);
 
-    const eaten_food = (player[0].x == healthy_food_x) && (player[0].y == healthy_food_y);
+    const eaten_food = (player[0].x == correctPaintX) && (player[0].y == correctPaintY);
     if (eaten_food) {
         //score
         score += 1;
@@ -270,8 +275,9 @@ function has_game_ended() {
     //detect if the player hit itself 
     for (let i = 4; i < player.length; i++) {
 		if (player[i].x == player[0].x && player[i].y == player[0].y){
-		console.log("hit player")
-		return true}
+		    console.log("hit player")
+		    return true
+        }
     }
     //detect if the player hit left wall
     if (player[0].x < 0) {
@@ -294,7 +300,7 @@ function has_game_ended() {
         return true
     }
     //detect if the player ate the poison food
-    if ((player[0].x == poison_food_x) && (player[0].y == poison_food_y)){
+    if ((player[0].x == wrongPaintX) && (player[0].y == wrongPaintY)){
 		console.log("ate poison food")
         return true
     }
