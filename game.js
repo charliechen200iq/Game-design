9382148440a72c649e66aaf0dd14937b05ceabd6
Charlie Chen <chench2@whs.school.nz>
2022-07-01 08:56:19 +1200

    comments and double button bug

--- game.js
+++ game.js
@@ -5,6 +5,8 @@
 * Purpose: Design game
 **/
 
+
+
 // Some constants that will be used in this scripts
 // Width of the canvas 
 const WIDTH = 1000;
@@ -35,38 +37,47 @@ var player = [
 var ctx
 // The score of the game
 var score = 0;
-// The game runs on the interval 
+// The game changeDirections on the interval 
 var gameInterval;
+// Determines whether to change the direction of the player or not 
+// The player move every 100 milliseocnds, therefore the player can only change direction once every 100 milliseonds
+var changeDirection
 // Horizontal velocity of player
 var dx = squareWidth;
 // Vertical velocity of player
 var dy = 0;
-// Correct paint x position 
+// correct-paint x position 
 var correctPaintX;
-// Correct paint y position 
+// correct-paint y position 
 var correctPaintY;
-// Wrong paint x position 
+// wrong-paint x position 
 var wrongPaintX;
-// Wrong paint y position 
+// wrong-paint y position 
 var wrongPaintY;
 
 
-//When content loads run the function startCanvas
+// When content loads changeDirection the function startCanvas
 window.onload=startCanvas
-//This function is the starting screen of the game
+
+
+
+// This function is the starting screen of the game
 function startCanvas(){
     // Return a two dimensional drawing context
     ctx=document.getElementById("myCanvas").getContext("2d")
 
+    // Drawing instruction on the canvas
     clearCanvas();
     ctx.font = "40px comic"
     ctx.fillStyle = "black"
     ctx.textAlign = 'center';
-    ctx.fillText("Help the painter to fill the canvas with dark blue paint.", WIDTH/2, HEIGHT*3/10);
-    ctx.fillText("Avoid the light blue paint!", WIDTH/2, HEIGHT*4/10);
+    ctx.fillText("Help the painter to fill the canvas with dark blue paint", WIDTH/2, HEIGHT*3/10);
+    ctx.fillText("Avoid the light blue paint", WIDTH/2, HEIGHT*4/10);
+    ctx.fillText("Use wsad keys or arrows keys to move", WIDTH/2, HEIGHT*7/10);
 
     ctx.font = "70px comic"
-    ctx.fillText("Click Anywhere to Start Game", WIDTH/2, HEIGHT*3/5);
+    ctx.fillText("Click Anywhere to Start Game", WIDTH/2, HEIGHT*6/10);
+
 
     ctx.fillStyle = GAMECOLOUR["correctPaintColour"];
     ctx.strokestyle = GAMECOLOUR["correctPaintBorderColour"];
@@ -77,6 +88,7 @@ function startCanvas(){
     ctx.fillRect(720, 290, squareWidth, squareWidth);
     ctx.strokeRect(720, 290, squareWidth, squareWidth);
 
+    // Once click on the starting the screen, the game starts
     window.addEventListener('click', onClick);
     function onClick(){
         updateCanvas()
@@ -85,17 +97,20 @@ function startCanvas(){
 
 
 
+// This function changeDirections the games 
 function updateCanvas(){
-    get_healthy_food()
-    get_poison_food()
+    // Get the positon of the paint 
+    CorrectPaintPosition()
+    WrongPaintPosition()
 
-    //runs the game by making the snake move, and constantly check if it is dying 
+    // changeDirections the game by making the snake move every 100 milliseconds, and constantly check if it is dying 
     gameInterval = setInterval(()=> {
         if(has_game_ended() == true){
             gameEnd();
         } else {
+            changeDirection = true
             clearCanvas();
-            drawFood();
+            drawPaint();
             move_player();
             drawPlayer();
         }
@@ -104,18 +119,20 @@ function updateCanvas(){
 
 
 
+// This function is the ending screen of the game
 function gameEnd(){
-    //stop the game from running
+    // Stop the game from changeDirectionning
     clearCanvas(); 
     clearInterval(gameInterval)
-    console.log("you died!!!")
-
+    console.log("Game Over")
+    
+    // Inform the user that the game has ended, and instruction on how to restart
     ctx.font = "90px comic "
     ctx.fillStyle = "black"
     ctx.textAlign = 'center';
-    ctx.fillText("You died!", WIDTH/2, HEIGHT*2/5);
+    ctx.fillText("Game Over", WIDTH/2, HEIGHT*2/5);
     ctx.fillText("Click Anywhere to Restart", WIDTH/2, HEIGHT*3/5);
-
+    
     window.addEventListener('click',onClick);
     function onClick(){
         document.location.reload();
@@ -124,6 +141,7 @@ function gameEnd(){
 
 
 
+// This function clears the canvas
 function clearCanvas() {
     ctx.fillStyle="white"
     ctx.fillRect(0,0,WIDTH, HEIGHT)
@@ -131,57 +149,63 @@ function clearCanvas() {
 
 
 
-function random_food_postion(min, max){  
+// Run this function get a random sqaure position on the canvas
+function randowPaintPosition(min, max){  
     return Math.round(Math.random() * ((max - min)/ squareWidth)) * squareWidth;
 }
 
 
 
-function get_healthy_food() {
-    correctPaintX = random_food_postion(0, WIDTH - squareWidth);
+// This function gets the position for the correct-paint
+function CorrectPaintPosition() {
+    // Gets the position for the correct-paint
+    correctPaintX = randowPaintPosition(0, WIDTH - squareWidth);
     console.log(correctPaintX)
-    correctPaintY = random_food_postion(0, HEIGHT - squareWidth);
+    correctPaintY = randowPaintPosition(0, HEIGHT - squareWidth);
     console.log(correctPaintY)
 
-//make sure that the healthy food is not on the player
+    // Make sure that the correct-paint position is not on the player
     player.forEach(function food_on_player (part) {
         const on_player = (part.x == correctPaintX) && (part.y == correctPaintY)
         if (on_player) {
-            get_healthy_food()
+            CorrectPaintPosition()
         }
     });
 }
 
 
 
-function get_poison_food() {
-    wrongPaintX = random_food_postion(0, WIDTH - squareWidth);
+// This function gets the position for the wrong-paint
+function WrongPaintPosition() {
+    // Gets the position for the wrong-paint
+    wrongPaintX = randowPaintPosition(0, WIDTH - squareWidth);
     console.log(wrongPaintX)
-    wrongPaintY = random_food_postion(0, HEIGHT - squareWidth);
+    wrongPaintY = randowPaintPosition(0, HEIGHT - squareWidth);
     console.log(wrongPaintY)
 
-//make sure that the poison food is not on the player
+    // Make sure that the wrong-paint position is not on the player
     player.forEach(function food_on_player (part) {
         if ((part.x == wrongPaintX) && (part.y == wrongPaintY)) {
-           get_poison_food()
+           WrongPaintPosition()
         }
-   });
+    });
 
-//make sure that the poison food is not too close to player
+    // Make sure that the wrong-paint position is not too close to the player, so that the user is capable of avoiding it
     if ((Math.abs(player[0].x - wrongPaintX) < squareWidth*5 ) || (Math.abs(player[0].y - wrongPaintY) < squareWidth*5)) {
         console.log("too close")
-        get_poison_food()
+        WrongPaintPosition()
     }
 
-//makesure thatthe poison food is not on the healthy food 
+    // Make sure that the wrong-paint position is not the same as the correct-paint position
     if ((wrongPaintX == correctPaintX) && (wrongPaintY == correctPaintY)){
-        get_poison_food()
+        WrongPaintPosition()
     }
 }
 
 
 
-function drawFood() {
+// This function draws the paint on the canvas
+function drawPaint() {
     ctx.fillStyle = GAMECOLOUR["correctPaintColour"];
     ctx.strokestyle = GAMECOLOUR["correctPaintBorderColour"];
     ctx.fillRect(correctPaintX, correctPaintY, squareWidth, squareWidth);
@@ -194,16 +218,18 @@ function drawFood() {
 
 
 
+// This function draws the player
 function drawPlayer(){
     player.forEach(drawPlayerPart)
 }
+// This function draws each part of the player
 function drawPlayerPart(part){
     // Set the colour of the player part
     ctx.fillStyle = GAMECOLOUR["playerColour"];
     // Set the border colour of the player part
     ctx.strokestyle = GAMECOLOUR["playerBorderColour"];
     // Draw a "filled" rectangle to represent the player part at the coordinates
-    // the part is located
+    // The part is located
     ctx.fillRect(part.x, part.y, squareWidth, squareWidth);
     // Draw a border around the player part
     ctx.strokeRect(part.x, part.y, squareWidth, squareWidth);
@@ -211,97 +237,109 @@ function drawPlayerPart(part){
 
 
 
+// Event listener runs the keyDownFunction every time a key is being pressed
 window.addEventListener('keydown', keyDownFunction)
+// This function changes the direction of the player 
 function keyDownFunction(event) {
-    //Get the key
+    if (changeDirection == true) {
+    // Get the key
     var keyDown = event.key
 
-    // Prevent the snake from reversing
+    // Prevent the player from reversing
     const goingUp = dy === -squareWidth
     const goingDown = dy === squareWidth
     const goingRight = dx === squareWidth
     const goingLeft = dx === -squareWidth
 
-    //change the direction 
+    // Change the direction 
     if ((keyDown === "ArrowLeft" || keyDown === "a") && !goingRight) {
         dx = -squareWidth
         dy = 0
-       // setTimeout(keyDownFunction, 100, event)
-
+        // Prevent the player from changing the direction again before the player changed the direction on the canvas
+        changeDirection = false
     }
     if ((keyDown === "ArrowUp" || keyDown === "w") && !goingDown) {
         dx = 0
         dy = -squareWidth
-        //setTimeout(keyDownFunction, 100, event)
+        // Prevent the player from changing the direction again before the player changed the direction on the canvas
+        changeDirection = false
     }
     if ((keyDown === "ArrowRight" || keyDown === "d") && !goingLeft){
         dx = squareWidth
         dy = 0
-        //setTimeout(keyDownFunction, 100, event)
+        // Prevent the player from changing the direction again before the player changed the direction on the canvas
+        changeDirection = false
     }
     if ((keyDown === "ArrowDown" || keyDown === "s") && !goingUp) {
         dx = 0
         dy = squareWidth
-       //setTimeout(keyDownFunction, 100, event)
+        // Prevent the player from changing the direction again before the player changed the direction on the canvas
+        changeDirection = false
+    }
     }
 }
 
 
 
+// This function moves the players based on the direction
 function move_player() {
-    // Create the new player's head
-    const head = {x: player[0].x + dx, y: player[0].y + dy};
-    // Add the new head to the beginning of snake body
-    player.unshift(head);
+    // Create the new player's head position
+    const HEAD = {x: player[0].x + dx, y: player[0].y + dy};
+    // Add the new head to the beginning of player's body to move the player
+    player.unshift(HEAD);
 
-    const eaten_food = (player[0].x == correctPaintX) && (player[0].y == correctPaintY);
-    if (eaten_food) {
-        //score
+    // Checks if the correct-paint has being captured
+    const getCorrectPaint = (player[0].x == correctPaintX) && (player[0].y == correctPaintY);
+
+    if (getCorrectPaint) {
+        // If correct-paint has being captured: increase the score, generate new postion for correct-paint and wrong-paint
+        // Also we don't remove the last sqaure of the player to increase the length of the player (added the new head)
         score += 1;
         document.getElementById('score').innerHTML = "Score: " + score;
 
-        // Generate new food location
-        get_healthy_food();
-        get_poison_food();
+        CorrectPaintPosition();
+        WrongPaintPosition();
     } else {
-        // Remove the last part of snake body
+        // if correct-paint hasn't being captured we remove the last square of the player so that the player stay the same length
         player.pop();
     }
 }
 
 
 
+// This function checks if the player has died
 function has_game_ended() {
-    //detect if the player hit itself 
+    // Detects if the player hit itself 
     for (let i = 4; i < player.length; i++) {
 		if (player[i].x == player[0].x && player[i].y == player[0].y){
 		    console.log("hit player")
 		    return true
         }
     }
-    //detect if the player hit left wall
+    // Detects if the player hit left wall
     if (player[0].x < 0) {
 		console.log("hit left wall")
     	return true
     } 
-    //d etect if the pplayer hit the right wall
+    // Detects if the pplayer hit the right wall
     if (player[0].x + squareWidth > WIDTH) {
 		console.log("hit right wall")
         return true
     }
-    //detect if the player hit the upper wall
+    // Detects if the player hit the upper wall
     if (player[0].y  < 0) {
 		console.log("hit upper wall")
         return true
     }
-    //detect if the player hit the bottom wall
+    // Detects if the player hit the bottom wall
     if (player[0].y + squareWidth > HEIGHT) {
 		console.log("hit bottom wall")
         return true
     }
-    //detect if the player ate the poison food
+    // Detects if the player ate the poison food
     if ((player[0].x == wrongPaintX) && (player[0].y == wrongPaintY)){
-		console.log("ate poison food")
+		console.log("wrong-paint")
         return true
     }
 }
+
